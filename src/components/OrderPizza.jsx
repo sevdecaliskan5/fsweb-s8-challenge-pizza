cdc2287074647b1f30157ecc828f4a8b9d994178
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-02-22 11:09:16 +0300

    OrderPizza icin import kismi tamamlandi

---
+++ src/components/OrderPizza.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect , useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { Form, FormGroup, Label, Input, Button, FormText, FormFeedback } from "reactstrap";
+import LogoSVG from "./images/iteration-1-images/logo.svg";
