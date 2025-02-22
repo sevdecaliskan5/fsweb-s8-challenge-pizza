9aefac65d6dfaa5356bc560e4855dc58c797f3ce
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-02-22 14:44:50 +0300

    OrderPizza sayfasinin css kodlari tamamlandi

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import OrderPizza from "./components/OrderPizza";
-import Home from "./components/Home";
 import MainPage from "./components/MainPage";
 import Success from "./components/Success";
 import Footer from "./components/Footer";
