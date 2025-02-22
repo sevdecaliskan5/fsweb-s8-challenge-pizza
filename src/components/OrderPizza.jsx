15cea765bda2674f39fac3626e2d960d1deb8fc3
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-02-22 11:15:06 +0300

    Kullanicidan alinacak veriler objesi tanimlandi

--- src/components/OrderPizza.jsx
+++ src/components/OrderPizza.jsx
@@ -3,3 +3,13 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { Form, FormGroup, Label, Input, Button, FormText, FormFeedback } from "reactstrap";
 import LogoSVG from "./images/iteration-1-images/logo.svg";
+
+
+// kullanicidan alinacak veriler objesi tanimlandi
+const initialData = {
+    boyut: "",
+    hamur: "",
+    malzemeler: [],
+    siparisNotu: "",
+    miktar: ""
+};
