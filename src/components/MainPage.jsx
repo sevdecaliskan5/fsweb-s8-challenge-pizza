9d82fceb2d550d9e79d97a4ad659a5c1c660a4f3
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-02-22 15:37:57 +0300

    Home componenti olusturuldu

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -10,8 +10,8 @@ import {
   CardTitle,
   CardText,
 } from "reactstrap";
+import Home from "./Home";
 import Footer from "./Footer";
-
 
 export default function MainPage() {
   const history = useHistory();
@@ -22,31 +22,7 @@ export default function MainPage() {
 
   return (
     <>
-      <div
-        style={{
-          backgroundImage: `url("/images/iteration-1-images/home-banner.png")`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          width: "100%",
-          height: "100vh",
-          textAlign: "center",
-          color: "white",
-          paddingTop: "20px",
-        }}
-      >
-        <img src={LogoSVG} alt="Logo" />
-        <h3 className="firsat">fırsatı kaçırma</h3>
-        <h1 className="main-title">
-          KOD ACIKTIRIR <br /> PİZZA, DOYURUR
-        </h1>
-        <button
-          className="anasayfa-buton"
-          data-cy="mainpage-button"
-          onClick={handleButton}
-        >
-          ACIKTIM
-        </button>
-      </div>
+      <Home />
       <nav className="main-nav">
         <ul>
           <li>
@@ -69,19 +45,28 @@ export default function MainPage() {
           </li>
           <li>
             <a href="/">
-              <img src="/images/iteration-2-images/icons/4.svg" alt="Kızartmalar" />
+              <img
+                src="/images/iteration-2-images/icons/4.svg"
+                alt="Kızartmalar"
+              />
               Kızartmalar
             </a>
           </li>
           <li>
             <a href="/">
-              <img src="/images/iteration-2-images/icons/5.svg" alt="Fast Food" />
+              <img
+                src="/images/iteration-2-images/icons/5.svg"
+                alt="Fast Food"
+              />
               Fast Food
             </a>
           </li>
           <li>
             <a href="/">
-              <img src="/images/iteration-2-images/icons/6.svg" alt="Gazlı İçecek" />
+              <img
+                src="/images/iteration-2-images/icons/6.svg"
+                alt="Gazlı İçecek"
+              />
               Gazlı İçecek
             </a>
           </li>
@@ -90,7 +75,10 @@ export default function MainPage() {
       <main className="main-page">
         <section className="cards">
           <div className="card-1">
-            <img src="/images/iteration-2-images/cta/kart-1.png" alt="İlk Kart" />
+            <img
+              src="/images/iteration-2-images/cta/kart-1.png"
+              alt="İlk Kart"
+            />
             <div className="overlay-content">
               <h1 className="card-title">
                 Özel <br /> Lezzetus
@@ -102,7 +90,10 @@ export default function MainPage() {
             </div>
           </div>
           <div className="card-2">
-            <img src="/images/iteration-2-images/cta/kart-2.png" alt="İkinci Kart" />
+            <img
+              src="/images/iteration-2-images/cta/kart-2.png"
+              alt="İkinci Kart"
+            />
             <div className="overlay-content">
               <h1 className="card-title">
                 Hackhathlon <br />
@@ -114,7 +105,10 @@ export default function MainPage() {
             </div>
           </div>
           <div className="card-2">
-            <img src="/images/iteration-2-images/cta/kart-3.png" alt="İkinci Kart" />
+            <img
+              src="/images/iteration-2-images/cta/kart-3.png"
+              alt="İkinci Kart"
+            />
             <div className="overlay-content">
               <h1 className="card-title">
                 <span>Çoooook hızlı </span>
@@ -133,19 +127,28 @@ export default function MainPage() {
             <ul>
               <li>
                 <a href="/">
-                  <img src="/images/iteration-2-images/icons/1.svg" alt="Kore" />
+                  <img
+                    src="/images/iteration-2-images/icons/1.svg"
+                    alt="Kore"
+                  />
                   YENİ! Kore
                 </a>
               </li>
               <li>
                 <a href="/">
-                  <img src="/images/iteration-2-images/icons/2.svg" alt="Pizza" />
+                  <img
+                    src="/images/iteration-2-images/icons/2.svg"
+                    alt="Pizza"
+                  />
                   Pizza
                 </a>
               </li>
               <li>
                 <a href="/">
-                  <img src="/images/iteration-2-images/icons/3.svg" alt="Burger" />
+                  <img
+                    src="/images/iteration-2-images/icons/3.svg"
+                    alt="Burger"
+                  />
                   Burger
                 </a>
               </li>
@@ -160,7 +163,10 @@ export default function MainPage() {
               </li>
               <li>
                 <a href="/">
-                  <img src="/images/iteration-2-images/icons/5.svg" alt="Fast Food" />
+                  <img
+                    src="/images/iteration-2-images/icons/5.svg"
+                    alt="Fast Food"
+                  />
                   Fast Food
                 </a>
               </li>
